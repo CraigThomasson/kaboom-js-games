8982cebef85762c8daf9eb1f099453f5f86d2c57
CraigThomasson <c.a.thomasson@hotmail.co.uk>
2022-11-14 16:45:31 +0000

    add game.js and basic bootstrap cards to index.html

---
+++ game.js
@@ -0,0 +1,70 @@
+
+
+    // import kaboom lib
+import kaboom from "https://unpkg.com/kaboom/dist/kaboom.mjs";
+    
+kaboom({
+    width: 700,
+    height: 500,
+    font: "sinko",
+    canvas: document.querySelector("#mycanvas"),
+    background: [ 15, 33, 12, ],
+})
+
+loadSprite("bean", "sprites/wizards-f.png")
+loadSprite("ground", "sprites/ground.png")
+// add a character to screen
+const player = add([
+// list of components
+sprite("bean"),
+pos(80, 40),
+area(),
+body(),
+]);
+
+const MOVE_SPEAD = 200
+
+
+// player movement
+player.onUpdate(() => {
+    camPos(player.pos)
+})
+
+onKeyDown("a", () => {
+    player.move(-MOVE_SPEAD, 0)
+})
+
+onKeyDown("d", () => {
+    player.move(MOVE_SPEAD, 0)
+})
+
+onKeyPress("w", () => {
+    if(player.isGrounded()) {
+        player.jump()
+    }
+})
+
+// maps
+
+addLevel([
+'                    ',
+'                    ',
+'              xxx   ',
+'       xxx          ',
+'              xxx   ',
+'       xxx          ',
+'              xxx   ',
+'       xxx          ',
+'              xxx   ',
+'       xxx          ',
+'  xxxxxxxxxxxx      ',
+], {
+width: 31,
+height: 100,
+"x": () => [
+    sprite("ground"),
+    area(),
+    solid(),
+],
+})
+
